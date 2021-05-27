414810c4f0e1a82db4db1a41179960204b2c7cea
Jingu Kang <jinguk0816@gmail.com>
2021-05-27 13:08:34 -0400

    add category controller for admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @admin_categories = Category.order(id: :desc).all
+    @product_count = Product.group(:category_id).count
+    p @product_count
+  end
+
+  def new
+    @admin_category = Category.new
+  end
+
+  def create
+    @admin_category = Category.new(category_params)
+
+    if @admin_category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
